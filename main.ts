57c3b76403181a6350ac46302498dde4cf6ec8b8
theRealOKo86 <98351920+theRealOKo86@users.noreply.github.com>
2025-08-31 14:00:09 +0200

    Added mode switching and treasure hunt mode

--- main.ts
+++ main.ts
@@ -1,50 +1,97 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (1 == receivedNumber) {
-        rotate = 0
-        speed = 2
-        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
-    } else if (2 == receivedNumber) {
-        rotate = 0
-        speed = 2
-        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
-    } else {
-    	
+    if (0 == modes_array.indexOf(mode)) {
+        if (1 == receivedNumber) {
+            rotate = 0
+            speed = 2
+            strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+        } else if (2 == receivedNumber) {
+            rotate = 0
+            speed = 2
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+        }
+    } else if (1 == modes_array.indexOf(mode)) {
+        delta = Math.map(Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength)), 55, 95, 25, 500)
+        if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -90) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -90 && radio.receivedPacket(RadioPacketProperty.SignalStrength) < -80) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -80 && radio.receivedPacket(RadioPacketProperty.SignalStrength) < -75) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+        } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -75 && radio.receivedPacket(RadioPacketProperty.SignalStrength) < -60) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -60) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        }
+    }
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    if (input.isGesture(Gesture.ScreenDown)) {
+        mode = modes_array[(modes_array.indexOf(mode) + 1) % modes_array.length]
+        music.play(music.tonePlayable(tones_array[modes_array.indexOf(mode)], music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        if (0 == modes_array.indexOf(mode)) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+            rotate = 0
+            speed = 2
+        } else if (1 == modes_array.indexOf(mode)) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+            delta = 1000
+        }
+        led.stopAnimation()
+        basic.showString("" + (mode))
     }
 })
 input.onButtonPressed(Button.A, function () {
-    if (0 == rotate) {
-        strip.rotate(1)
-        strip.show()
-    } else {
-        speed = speed - 1
+    if (0 == modes_array.indexOf(mode)) {
+        if (0 == rotate) {
+            strip.rotate(1)
+            strip.show()
+        } else {
+            speed = speed - 1
+        }
+    } else if (1 == modes_array.indexOf(mode)) {
+    	
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    speed = Math.map(input.rotation(Rotation.Roll), -90, 90, -20, 20)
+input.onGesture(Gesture.SixG, function () {
+    if (0 == modes_array.indexOf(mode)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        rotate = 0
+        speed = 2
+    } else if (1 == modes_array.indexOf(mode)) {
+    	
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    if (0 == rotate) {
-        strip.setPixelColor(4, colors_array._pickRandom())
-        strip.show()
-    } else {
-        speed = speed + 1
+    if (0 == modes_array.indexOf(mode)) {
+        if (0 == rotate) {
+            strip.setPixelColor(4, colors_array._pickRandom())
+            strip.show()
+        } else {
+            speed = speed + 1
+        }
+    } else if (1 == modes_array.indexOf(mode)) {
+    	
     }
-})
-input.onGesture(Gesture.Shake, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    rotate = 0
-    speed = 2
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (0 == rotate) {
-        rotate = 1
-    } else {
-        rotate = 0
+    if (0 == modes_array.indexOf(mode)) {
+        if (0 == rotate) {
+            rotate = 1
+        } else {
+            rotate = 0
+        }
+        basic.pause(200)
+    } else if (1 == modes_array.indexOf(mode)) {
+    	
     }
-    basic.pause(200)
 })
+let delta = 0
+let mode = ""
+let tones_array: number[] = []
+let modes_array: string[] = []
 let colors_array: number[] = []
 let speed = 0
 let rotate = 0
@@ -66,17 +113,30 @@ neopixel.colors(NeoPixelColors.Purple),
 neopixel.colors(NeoPixelColors.White),
 neopixel.colors(NeoPixelColors.Black)
 ]
+modes_array = ["lightshow", "treasure_hunt"]
+tones_array = [262, 294]
+mode = modes_array[0]
+delta = 1000
 basic.forever(function () {
-    if (1 == rotate) {
-        if (speed > 0) {
-            strip.rotate(1)
-        } else {
-            strip.rotate(-1)
-        }
-        strip.show()
-        basic.pause(Math.abs(speed) * 25)
+    if (0 == modes_array.indexOf(mode)) {
+        basic.showString("Apalucha 2025")
+    } else if (1 == modes_array.indexOf(mode)) {
+    	
     }
 })
 basic.forever(function () {
-    basic.showString("Apalucha 2025")
+    if (0 == modes_array.indexOf(mode)) {
+        if (1 == rotate) {
+            if (speed > 0) {
+                strip.rotate(1)
+            } else {
+                strip.rotate(-1)
+            }
+            strip.show()
+            basic.pause(Math.abs(speed) * 25)
+        }
+    } else if (1 == modes_array.indexOf(mode)) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+        basic.pause(delta)
+    }
 })
